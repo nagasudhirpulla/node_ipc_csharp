87b13105a6a3652d3341985c33712e9d2ee4e834
nagasudhirpulla <nagasudhirpulla@gmail.com>
2019-09-08 19:13:35 +0530

    first commit

---
+++ IpcListenerApp/IpcListenerApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpcListenerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read arguments for meas Ids, from time, to time
+            string inpTimeFormat = "yyyy_MM_dd_HH_mm_ss";
+            DateTime fromTime = DateTime.Now.AddMinutes(-5);
+            DateTime toTime = fromTime.AddMinutes(0);
+            int measId = -1;
+            try
+            {
+                for (int argIter = 0; argIter < args.Length - 1; argIter++)
+                {
+                    Console.WriteLine(args[argIter]);
+                    if (args[argIter] == "--from_time")
+                    {
+                        fromTime = DateTime.ParseExact(args[argIter + 1], inpTimeFormat, CultureInfo.InvariantCulture);
+                    }
+                    if (args[argIter] == "--to_time")
+                    {
+                        toTime = DateTime.ParseExact(args[argIter + 1], inpTimeFormat, CultureInfo.InvariantCulture);
+                    }
+                    if (args[argIter] == "--meas_id")
+                    {
+                        measId = int.Parse(args[argIter + 1]);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // return nothing or send empty array                
+            }
+            Console.WriteLine($"fromTime = {fromTime.ToString()}, toTime = {toTime.ToString()}, measId = {measId}");
+            Console.Out.Flush();
+        }
+    }
+}
---
+++ node_app/index.ts
@@ -0,0 +1,34 @@
+const spawn = require('child_process').spawn;
+
+const make2Digits = (num: number): string => {
+    if (num <= 9 && num >= 0) {
+        return '0' + num;
+    }
+    return '' + num;
+};
+const convertTimeToInpStr = (time: Date): string => {
+    const yr = time.getFullYear();
+    const mon = make2Digits(time.getMonth() + 1);
+    const day = make2Digits(time.getDate());
+    const hr = make2Digits(time.getHours());
+    const mins = make2Digits(time.getMinutes());
+    const secs = make2Digits(time.getSeconds());
+    return `${yr}_${mon}_${day}_${hr}_${mins}_${secs}`;
+};
+const fromTimeStr = convertTimeToInpStr(new Date((new Date()).getTime() - 20 * 60 * 1000));
+const toTimeStr = convertTimeToInpStr(new Date((new Date()).getTime() - 10 * 60 * 1000));
+const measId = "222";
+
+const ipc = spawn(`./IpcListenerApp.exe`, ["--from_time", fromTimeStr, "--to_time", toTimeStr, "--meas_id", measId]);
+
+ipc.stderr.on('data', function (data) {
+    console.log(data.toString());
+});
+
+ipc.stdout.on('data', function (data) {
+    console.log(data.toString());
+});
+
+ipc.on('exit', (code) => {
+    console.log(`Ipc exe to exit with code: ${code}`);
+});
